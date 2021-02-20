afa4214fb8e9168785ec9f5b445a6d8d26dd78b3
CuteWisp <sweatpotato13@gmail.com>
2021-02-20 09:18:56 +0900

    feat: Add docker-compose for postgres

---
+++ postgres/updateConfig.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cat /tmp/postgresql.conf > /var/lib/postgresql/data/pgdata/postgresql.conf
+
+/etc/init.d/postgresql restart
+
---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { config as _config } from "dotenv";
+_config({ path: __dirname + "/../../.env" });
+
+export const config = {
+    // PostgreSQL
+    postgres_host: process.env.POSTGRES_HOST || "ship-postgres",
+    postgres_port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    postgres_password: process.env.POSTGRES_PASSWORD || "postgres",
+    postgres_db: process.env.POSTGRES_DB || "postgres",
+    postgres_volume: process.env.POSTGRES_VOLUME || "pgdata",
+    // PgAdmin
+    pgadmin_port: parseInt(process.env.PGADMIN_PORT) || 5050,
+    pgadmin_default_email: process.env.PGADMIN_DEFAULT_EMAIL || "admin@pgadmin.com",
+    pgadmin_default_password: process.env.PGADMIN_DEFAULT_PASSWORD || "admin",
+    // Hasura
+    hasura_port: parseInt(process.env.HASURA_PORT) || 8080,
+    hasura_admin_secret: process.env.HASURA_ADMIN_SECRET || "myadminsecretkey"
+};
